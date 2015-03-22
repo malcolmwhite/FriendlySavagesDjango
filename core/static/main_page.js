76dff5f90e4a2946eaab53ded11140b95e13f351
Malcolm White <malcolmdavidwhite@gmail.com>
2015-03-22 12:18:56 -0500

    Fixed ajax accordion stuff and added sound_bar html
    accordions need a foundation reflow
    
    the sound_bar isn't playing or sticking to the bottom

---
+++ core/static/main_page.js
@@ -0,0 +1,15 @@
+function ajaxifyLinks(){
+    // Capture all the links to push their url to the history stack and trigger the StateChange Event
+    $('a').not('.external_link').click(function(evt) {
+        // do not execute the actual link behaviour
+        evt.preventDefault();
+        History.pushState(null, $(this).text(), $(this).attr('href'));
+    });
+}
+ 
+function setActiveLink(selector){
+    $('#menu').find('a').removeClass('active');
+    if (selector){
+        $(selector).addClass('active');
+    }
+}
